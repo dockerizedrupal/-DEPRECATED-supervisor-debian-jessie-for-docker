750becff21cc252858cae28ade4bdd6a8dc683f2
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-19 07:37:29 +0300

    Update

--- src/build/etc/puppet/manifests/build.pp
+++ src/build/etc/puppet/manifests/build.pp
@@ -4,6 +4,11 @@ node default {
     source => '/tmp/build/etc/puppet/manifests/run.pp',
   }
 
+  file { '/etc/supervisor/conf.d/supervisord.conf':
+    ensure => present,
+    source => '/tmp/build/etc/supervisor/conf.d/supervisord.conf',
+  }
+
   class { 'apt':
     always_apt_update => true
   }
