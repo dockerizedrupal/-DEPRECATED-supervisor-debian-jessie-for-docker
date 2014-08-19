da61c360216ef8676350fa8a74abf1f1bb0af1ba
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-19 08:01:30 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -2,11 +2,12 @@ node default {
   file { '/opt/run.sh':
     ensure => present,
     source => '/tmp/build/run.sh',
+    mode => 755
   }
 
   file { '/etc/puppet/manifests/run.pp':
     ensure => present,
-    source => '/tmp/build/etc/puppet/manifests/run.pp',
+    source => '/tmp/build/etc/puppet/manifests/run.pp'
   }
 
   class { 'apt':
@@ -19,6 +20,6 @@ node default {
 
   file { '/etc/supervisor/conf.d/supervisord.conf':
     ensure => present,
-    source => '/tmp/build/etc/supervisor/conf.d/supervisord.conf',
+    source => '/tmp/build/etc/supervisor/conf.d/supervisord.conf'
   }
 }
