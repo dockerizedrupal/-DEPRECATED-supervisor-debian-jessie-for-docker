c4297631fb9397eb6271b8988306be5a0cb37da2
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 20:44:22 +0200

    Update

--- src/build/modules/supervisor/manifests/packages.pp
+++ src/build/modules/supervisor/manifests/packages.pp
@@ -1,13 +1,8 @@
 class supervisor::packages {
-  exec { 'apt-get update':
-    path => ['/usr/bin']
-  }
-
   package {[
       'supervisor',
       'python-pip'
     ]:
-    ensure => present,
-    require => Exec['apt-get update']
+    ensure => present
   }
 }
